ed28df192aa306a4d227c7ad5acdc07da42ae6f2
Esteban Rebuffé-Mareau <estebanianis@gmail.com>
2024-05-06 19:10:56 +0200

    init project

---
+++ backend/src/main/java/com/yourcaryourwaychat/YourcarYourwayChatApplication.java
@@ -0,0 +1,13 @@
+package com.yourcaryourwaychat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourcarYourwayChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YourcarYourwayChatApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/yourcaryourwaychat/YourcarYourwayChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yourcaryourwaychat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourcarYourwayChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
